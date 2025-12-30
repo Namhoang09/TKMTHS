e7492f3414a7ef8692b7ad9f932287da40c143e7
Đặng Hoàng Nam <138839936+Namhoang09@users.noreply.github.com>
2025-12-30 09:57:13 +0700

    Update ExpApprox.vhd

--- Behavioral/ExpApprox.vhd
+++ Behavioral/ExpApprox.vhd
@@ -46,55 +46,55 @@ ARCHITECTURE Behavioral OF ExpApprox IS
     
     	TYPE state_type IS (IDLE, INIT, CALC, FINISH);
     	SIGNAL current_state, next_state : state_type;
-	SIGNAL repeat_done : std_logic;
+	    SIGNAL repeat_done : std_logic;
 
 BEGIN
 
     	PROCESS(clk, rst)
     	BEGIN
         	IF (rst = '1') THEN
-            		current_state <= IDLE;
-			X <= (OTHERS => '0');
+                current_state <= IDLE;
+			    X <= (OTHERS => '0');
         		Y <= (OTHERS => '0');
         		Z <= (OTHERS => '0');
-            		i <= 1;
-			repeat_done <= '0';
+            	i <= 1;
+				repeat_done <= '0';
        		ELSIF (clk'EVENT AND clk = '1') THEN
             		IF (current_state = INIT) THEN
                 		IF (signed(t) = 0) THEN
-        				X <= to_signed(8192, DATA_WIDTH);
-        				Y <= (OTHERS => '0');
-        				Z <= (OTHERS => '0');
-        				i <= N; 
-    				ELSE
-        				X <= K_INV;           -- X = 1/K 
-        				Y <= (OTHERS => '0'); -- Y = 0
-        				Z <= signed(t);       -- Z = t
-        				i <= 1;
-    				END IF;
-				repeat_done <= '0';
+        					X <= to_signed(8192, DATA_WIDTH);
+        					Y <= (OTHERS => '0');
+        					Z <= (OTHERS => '0');
+        					i <= N; 
+    					ELSE
+        					X <= K_INV;           -- X = 1/K 
+        					Y <= (OTHERS => '0'); -- Y = 0
+        					Z <= signed(t);       -- Z = t
+        					i <= 1;
+    					END IF;
+						repeat_done <= '0';
             		ELSIF (current_state = CALC) THEN
-				X <= X_next;
+						X <= X_next;
                 		Y <= Y_next;
                 		Z <= Z_next;
 
-				IF ((i = 4 OR i = 13) AND repeat_done = '0') THEN
-					i <= i;
-					repeat_done <= '1';
-				ELSIF (i < N) THEN
-                    			i <= i + 1;
-					repeat_done <= '0';
+						IF ((i = 4 OR i = 13) AND repeat_done = '0') THEN
+							i <= i;
+							repeat_done <= '1';
+						ELSIF (i < N) THEN
+                    		i <= i + 1;
+							repeat_done <= '0';
                 		END IF;
             		END IF;
 
-			current_state <= next_state;
+					current_state <= next_state;
         	END IF;
     	END PROCESS;
 
     	PROCESS(current_state, start, i, X, Y, Z, t)
         	VARIABLE shift_x, shift_y : signed(DATA_WIDTH-1 DOWNTO 0);
     	BEGIN
-		next_state <= current_state;
+			next_state <= current_state;
         	done <= '0';
         	exp <= (OTHERS => '0');
         
@@ -114,26 +114,26 @@ BEGIN
             		WHEN INIT =>
                 		IF (signed(t) = 0) THEN
             				next_state <= FINISH;
-        			ELSE
+        				ELSE
             				next_state <= CALC;
-        			END IF;
+        				END IF;
 
             		WHEN CALC =>
-				IF (N = 4 OR N = 13) THEN
-					IF (i = N AND repeat_done = '1') THEN
-						next_state <= FINISH;
-					ELSE
-						next_state <= CALC;
-					END IF;
-				ELSE
-					IF (i = N) THEN
-						next_state <= FINISH;
-					ELSE
-						next_state <= CALC;
-					END IF;
-				END IF;
+						IF (N = 4 OR N = 13) THEN
+							IF (i = N AND repeat_done = '1') THEN
+								next_state <= FINISH;
+							ELSE
+								next_state <= CALC;
+							END IF;
+						ELSE
+							IF (i = N) THEN
+								next_state <= FINISH;
+							ELSE
+								next_state <= CALC;
+							END IF;
+						END IF;
 
-				IF (Z >= 0) THEN
+						IF (Z >= 0) THEN
                         		X_next <= X + shift_y;       	-- X + Y*2^-i
                         		Y_next <= Y + shift_x;       	-- Y + X*2^-i
                         		Z_next <= Z - LUT(i);  		-- Z - LUT[i]
@@ -141,11 +141,11 @@ BEGIN
                         		X_next <= X - shift_y;
                         		Y_next <= Y - shift_x;
                         		Z_next <= Z + LUT(i);
-                    		END IF;
+                    	END IF;
 
             		WHEN FINISH =>
-				done <= '1';
-				exp <= std_logic_vector(X + Y);
+						done <= '1';
+						exp <= std_logic_vector(X + Y);
                 		 
                 		IF (start = '0') THEN
                     			next_state <= IDLE;
@@ -153,4 +153,5 @@ BEGIN
         	END CASE;
     	END PROCESS;
 END Behavioral;
+
 
